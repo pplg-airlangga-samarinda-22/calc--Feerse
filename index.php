6524f2afa00f07d048ac50866f6c7ca047171d18
Feerse <sungaikeledang6@gmail.com>
2025-02-03 15:42:16 +0800

    Perbarui

--- index.php
+++ index.php
@@ -55,53 +55,59 @@
   </style>
 </head>
 
+<?php
+$num1 = $_POST['num1'] ?? '';
+$operation = $_POST['operation'] ?? '';
+$num2 = $_POST['num2'] ?? '';
+$result = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  switch ($operation) {
+    case 'tambah':
+      $result = $num1 + $num2;
+      break;
+    case 'kurang':
+      $result = $num1 - $num2;
+      break;
+    case 'kali':
+      $result = $num1 * $num2;
+      break;
+    case 'bagi':
+      $result = $num2 != 0 ? $num1 / $num2 : 'Tidak dapat dibagi dengan nol.';
+      break;
+  }
+
+  if (isset($_POST['isNum1InputResult'])) {
+    $num1 = $result;
+  } else {
+    $num1 = '';
+  }
+}
+?>
+
 <body>
   <h1>Kalkulator Sederhana</h1>
   <form method="post">
-    <input type="number" name="num1" placeholder="Angka pertama" required>
-    <select name="operation">
+    <input step="any" type="number" value="<?= $num1 ?>" name="num1" placeholder="Angka pertama" required />
+    <select name="operation" required>
       <option value="" disabled selected>Pilih operasi</option>
-      <option value="tambah">+</option>
-      <option value="kurang">-</option>
-      <option value="kali">*</option>
-      <option value="bagi">/</option>
+      <option value="tambah">Tambah (+)</option>
+      <option value="kurang">Kurang (-)</option>
+      <option value="kali">Kali (*)</option>
+      <option value="bagi">Bagi (/)</option>
     </select>
-    <input type="number" name="num2" placeholder="Angka kedua" required>
-    <button type="submit" name="submit" value="calculate">Hitung</button>
+    <input step="any" type="number" name="num2" placeholder="Angka kedua" required />
+    <div>
+      <input type="checkbox" name="isNum1InputResult" id="isNum1InputResult" />
+      <label for="isNum1InputResult">Isi otomatis bilangan pertama berdasarkan hasil</label>
+    </div>
+    <br>
+    <button type="submit">Hitung</button>
   </form>
 
-  <?php
-  if (isset($_POST['submit'])) {
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $operation = $_POST['operation'];
-    $result = 0;
-
-    switch ($operation) {
-      case 'tambah':
-        $result = $num1 + $num2;
-        break;
-      case 'kurang':
-        $result = $num1 - $num2;
-        break;
-      case 'kali':
-        $result = $num1 * $num2;
-        break;
-      case 'bagi':
-        if ($num2 != 0) {
-          $result = $num1 / $num2;
-        } else {
-          $result = "Tidak bisa membagi dengan nol";
-        }
-        break;
-      default:
-        $result = "Operasi tidak valid";
-        break;
-    }
-
-    echo "<h2>Hasil: $result</h2>";
-  }
-  ?>
+  <?php if ($result !== ''): ?>
+    <h2>Hasil: <?= $result ?></h2>
+  <?php endif; ?>
 </body>
 
 </html>
